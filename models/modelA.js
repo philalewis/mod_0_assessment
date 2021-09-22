0adbdffd614cde92940aa40f2c82c177857c57cb
Phil Lewis <phillewis1985@gmail.com>
2021-09-22 12:52:43 -0500

    Add variable to modelA.js

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  name: "T-Rex",
+  diet: "meat",
+  height: 20,
+  weight: 10000,
+  isAggressive: true,
+  extinct: true,
+  favoriteFoods: ["Brontosaurus", "Stegasaurus"],
+  featuredIn: ["Jurrasic Park", "Jurrasic Park II"]
+}
