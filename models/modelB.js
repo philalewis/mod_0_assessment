f77ec3c37828cfefbabb37a60cef82aeb1defef8
Phil Lewis <phillewis1985@gmail.com>
2021-09-22 12:58:47 -0500

    Add object to modelB.js

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  name: "Phil",
+  lastName: "Lewis",
+  age: 36,
+  degrees: 2,
+  isExcited: true,
+  knowsEverything: false,
+  devTypes: ["Front End", "Back End"],
+  placesLived: ["Oklahoma City", "Norman"]
+}
