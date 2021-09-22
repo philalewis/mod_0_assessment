85e938ecb6cafabbad071d896ccd3ba6358ea665
Phil Lewis <phillewis1985@gmail.com>
2021-09-22 12:46:00 -0500

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var movie = "Happy Gilmore";
+var isEntertaining = true;
+movie = "Billy Madison";
+var num4 = 4;
+var num6 = 6;
+var product = num4 * num6;
+var favoriteFoods = ["Pizza", "Sushi", "Gumbo"];
+favoriteFoods.shift();
+console.log(movie.length);
